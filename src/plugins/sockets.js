c479b7b435de2026d96d7c44280c308012024d99
Hidde-Jan Jongsma <hidde-jan.jongsma@tno.nl>
2020-06-03 16:10:34 +0200

    Use default socket (same host as frontend)

--- src/plugins/sockets.js
+++ src/plugins/sockets.js
@@ -2,6 +2,6 @@ import Vue from "vue";
 import VueSocketIOExt from "vue-socket.io-extended";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const socket = io();
 
 Vue.use(VueSocketIOExt, socket);
